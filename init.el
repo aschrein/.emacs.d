8c249b58ffeea47db8db2609db448dddcb45ac0e
Anton Schreiner <antonschrein@gmail.com>
2019-03-03 13:29:45 +0000

    Add swap/move window

--- init.el
+++ init.el
@@ -4,7 +4,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (atom-one-dark-theme atom-dark-theme exwm))))
+ '(package-selected-packages
+   (quote
+    (## buffer-move atom-one-dark-theme atom-dark-theme exwm))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -16,6 +18,7 @@
 (package-initialize)
 (require 'exwm)
 (require 'exwm-config)
+(require 'buffer-move)
 (exwm-config-default)
 ;(require 'atom-one-dark-theme)
 (load-theme 'atom-one-dark t)
@@ -49,4 +52,41 @@
 (exwm-input-set-key (kbd "s-2") 'aschrein/exwm-line-mode)
 (exwm-input-set-key (kbd "s-3") 'aschrein/exwm-nomode)
 
+(global-set-key (kbd "s-k") #'windmove-down)
+(global-set-key (kbd "s-i") #'windmove-up)
+(global-set-key (kbd "s-j") #'windmove-left)
+(global-set-key (kbd "s-l") #'windmove-right)
+
 (exwm-input-set-key (kbd "s-f") #'exwm-layout-toggle-fullscreen)
+(exwm-input-set-key (kbd "s-l") #'windmove-right)
+(exwm-input-set-key (kbd "s-j") #'windmove-left)
+(exwm-input-set-key (kbd "s-i") #'windmove-up)
+(exwm-input-set-key (kbd "s-k") #'windmove-down)
+
+(set-face-attribute 'default nil
+                    :family "Source Code Pro"
+                    :height 110
+                    :weight 'normal
+                    :width 'normal)
+
+
+(show-paren-mode 1)
+
+(exwm-input-set-key (kbd "s-<right>") #'buf-move-right)
+(exwm-input-set-key (kbd "s-<left>") #'buf-move-left)
+(exwm-input-set-key (kbd "s-<up>") #'buf-move-up)
+(exwm-input-set-key (kbd "s-<down>") #'buf-move-down)
+;(split-window-below)
+;(split-window-right)
+;(global-set-key (kbd "H-o") #'other-window)
+(defun exec-current-file ()
+  "run a command on the current file"
+  (interactive)
+  (save-buffer)
+  (shell-command 
+   (format "sbcl --script %s" 
+	   (shell-quote-argument (buffer-file-name))
+	   )
+   )
+  )
+(global-set-key (kbd "s-e") 'call-something-on-current-buffers-file)
